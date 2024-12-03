775024e08ade1a183c546edc422e4142e0c69044
Navneet-Scaler <navneet.23bcs10136@ms.sst.scaler.com>
2024-12-03 15:48:12 +0530

    Boolean Algebra Repo Initiated Boolean Variable File Added

---
+++ ReadME.md
@@ -0,0 +1,2 @@
+# Boolean Algebra Topics 
+1. [Boolean variables](../08_Boolean_Algebra/bool1.py) 
---
+++ bool1.py
@@ -0,0 +1,22 @@
+"""
+Topic 1. Boolean Variable
+Some examples of Boolean variables are true/false, 1/0, yes/no, and data OR intelligence.
+"""
+
+"""
+20 Problems
+"""
+
+"""
+Problem 1) Is Number Even?
+"""
+# num = int(input("Enter your number: "))
+# if num%2==0:
+#     print("Yes! Even")
+# else:
+#     print("No! Odd")
+
+"""
+Problem 2) Is number positive
+"""
+num = int(input("Enter your number: "))
